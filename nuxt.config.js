0d5b4efcc8a1f9584ec51cc1c7a6eb8592354614
Mubarak SULAYMAN <mubaracksulaymann@gmail.com>
2022-02-15 12:19:56 +0100

    UPDATE: Introduce generate property to nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,9 @@
 export default {
   generate: {
-    fallback: true
+    fallback: true,
+    routes: ['']
   },
-  
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: 'Fidia - %s',
